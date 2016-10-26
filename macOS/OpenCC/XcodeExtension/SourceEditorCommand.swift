fc26aba74b28386336e48bee1c42b1ccf015296f
钟颖 <cyan.zy@alibaba-inc.com>
2016-10-26 11:26:25 +0800

    Add Xcode Extension

---
+++ macOS/OpenCC/XcodeExtension/SourceEditorCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  XcodeExtension
+//
+//  Created by cyan on 2016/10/26.
+//  Copyright © 2016年 cyan. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}

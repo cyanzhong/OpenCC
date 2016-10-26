d4975ae87be899ec5358b2f1583a458cb6ae67b6
钟颖 <cyan.zy@alibaba-inc.com>
2016-10-26 11:17:56 +0800

    Add macOS project

---
+++ macOS/OpenCC/OpenCC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenCC
+//
+//  Created by cyan on 2016/10/26.
+//  Copyright © 2016年 cyan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ macOS/OpenCC/OpenCCKit/OpenCCKit.h
@@ -0,0 +1,19 @@
+//
+//  OpenCCKit.h
+//  OpenCCKit
+//
+//  Created by cyan on 2016/10/26.
+//  Copyright © 2016年 cyan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OpenCCKit.
+FOUNDATION_EXPORT double OpenCCKitVersionNumber;
+
+//! Project version string for OpenCCKit.
+FOUNDATION_EXPORT const unsigned char OpenCCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenCCKit/PublicHeader.h>
+
+
